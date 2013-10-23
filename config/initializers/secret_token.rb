e79b52713c24a6b185f5016b93f47d469e7eccbe
Ludvig Rhodin <lrhodin@gmail.com>
2013-10-23 02:58:22 -0600

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beesneeds::Application.config.secret_key_base = '523fec19405cb1097465e24b78f2d9133a8bdf2d028f375a1284b61fb1e78868b22037a50cf39135efa22eed21fa4b428fa3d8f6379da99e539b128f0a55ab4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Beesneeds::Application.config.session_store :cookie_store, key: '_beesneeds_session'
